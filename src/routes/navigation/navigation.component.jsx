76431f7c46a1d6a96b5e451dd9d78e46ed70c22f
Harmeet Singh <harmeet.sandal@gmail.com>
2022-09-13 00:49:16 +0530

    add to cart component added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,11 +4,14 @@ import { useContext } from "react"
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg"
 import './navigation.styles.scss'
 import { UserContext } from "../../contexts/user.context"
+import { CartContext } from "../../contexts/cart.context"
 import { signOutUser } from "../../utils/firebase/firebase.util"
+import CartIcon from "../../components/cart-icon/cart-icon.component"
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 const Navigation = () => {
 
     const { currentUser } = useContext(UserContext)
-
+    const { isCartOpen } = useContext(CartContext)
     return (
         <>
             <div className="navigation">
@@ -27,8 +30,10 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     }
-
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
+
             </div>
             <Outlet />
         </>
