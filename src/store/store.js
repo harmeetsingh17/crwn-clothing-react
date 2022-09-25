2f127429fa7b771b3cf5fd913afd0f05302277eb
Harmeet Singh <harmeet.sandal@gmail.com>
2022-09-25 15:48:00 +0530

    redux extension added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux'
-
+import { persistStore, persistReducer } from 'redux-persist'
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+import storage from 'redux-persist/lib/storage';
 
 const loggerMiddleware = (store) => (next) => (action) => {
     if (!action.type) {
@@ -11,8 +12,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
     next(action)
 }
 
-const middleware = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
 
-const composedEnhancers = compose(applyMiddleware(...middleware))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const middleware = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+
+const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleware))
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store)
