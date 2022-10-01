1df4c9e242b8adb3bd02f1f46535374a348d0870
Harmeet Singh <harmeet.sandal@gmail.com>
2022-10-01 15:59:29 +0530

    sign up added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js'
+
+import { stripePromise } from './utils/stripe/stripe.utils';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,7 +17,9 @@ root.render(
     <Provider store={store} >
       <PersistGate loading={null} persistor={persistor} >
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
